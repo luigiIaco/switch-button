2269215187a0ab4dcc121007755d2a4d01f439ee
luigiIaco <157249321+luigiIaco@users.noreply.github.com>
2024-01-26 23:51:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let bool=true;
+let x=document.getElementById("elemento");
+let y=document.getElementById("sfondo");
+let z=document.getElementById("sco");
+x.addEventListener("click",function() {
+    if(bool==true) {
+    x.style.backgroundImage="url(img/bi.png)";
+    x.style.border='0px solid transparent';
+    x.style.left='70px';
+    y.style.backgroundImage='url(img/notte1.jpg)';
+    document.body.style.backgroundImage='url(img/mare.jpg)';
+    bool=false;
+    }
+    else {
+    x.style.backgroundImage="url(img/sole.jpg)";
+    x.style.border='1px solid white';
+    x.style.left='0px';
+    y.style.backgroundImage='url(img/cielo.jpg)';
+    document.body.style.backgroundImage='url(img/palme.jpg)';
+    bool=true;
+    }
+})
